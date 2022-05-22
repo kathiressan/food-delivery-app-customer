fd22d4f34a7e635196b08f0640e7ebefe8cd40c5
Kathiressan Sivanes <kathiressan@lifelinelab.io>
2022-05-22 22:02:56 +0800

    latest changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -30,42 +30,86 @@ import PopularProductComponent from "../components/PopularProductComponent";
 const HomeScreen = () => {
   const navigation = useNavigation();
   const [products, setProducts] = useState([]);
+
   const DATA = [
     {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      id: "1",
       image:
-        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-1foodgroups_dairy_detailfeature.jpg?sfvrsn=23510b0_6",
+        "https://focus.cbbc.org/wp-content/uploads/2020/08/baby_food_sector-1.jpg",
+      categoryName: "Baby",
+    },
+    {
+      id: "2",
+      image:
+        "https://i2.wp.com/media.premiumtimesng.com/wp-content/files/2022/01/Mashed.jpg?fit=1600%2C900&ssl=1",
+      categoryName: "Beverages",
+    },
+    {
+      id: "3",
+      image:
+        "https://www.snackandbakery.com/ext/resources/images/bakeryproducts.jpg?1432238179",
+      categoryName: "Bread & Bakery",
+    },
+    {
+      id: "4",
+      image:
+        "https://www.verywellhealth.com/thmb/LiEYTC9ZucAPtgIkxivHFQQpXUY=/1333x1000/smart/filters:no_upscale()/30D7A016-ABA5-48DD-BE39-3E7A223A03BF-96f2ba9e6c724dc9b2ba638b0c0f44a2.jpeg",
+      categoryName: "Breakfast & Cereal",
+    },
+    {
+      id: "5",
+      image:
+        "https://static.vecteezy.com/system/resources/thumbnails/000/366/324/small/ugx4_n9k4_170201.jpg",
+      categoryName: "Canned Goods",
+    },
+    {
+      id: "6",
+      image:
+        "https://cdn11.bigcommerce.com/s-p891vzg/images/stencil/original/uploaded_images/types-of-spices-that-are-the-most-popular-right-now.jpg?t=1517279969",
+      categoryName: "Condiments/Spices",
+    },
+    {
+      id: "7",
+      image:
+        "https://c4.wallpaperflare.com/wallpaper/677/705/249/color-cookies-candy-sweets-wallpaper-preview.jpg",
+      categoryName: "Snacks & Candy",
+    },
+
+    {
+      id: "8",
+      image:
+        "https://domf5oio6qrcr.cloudfront.net/medialibrary/9685/iStock-544807136.jpg",
       categoryName: "Dairy",
     },
     {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bb",
+      id: "9",
       image:
-        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-1foodgroups_dairy_detailfeature.jpg?sfvrsn=23510b0_6",
-      categoryName: "Dairy",
+        "https://www.news-medical.net/image.axd?picture=2020%2F1%2Fshutterstock_321864554.jpg",
+      categoryName: "Produce",
     },
     {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bc",
+      id: "10",
       image:
-        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-1foodgroups_dairy_detailfeature.jpg?sfvrsn=23510b0_6",
-      categoryName: "Dairy",
+        "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/FB466C96-E47A-4C5E-A287-AB192C6CA9C0/Derivates/A6128AA4-AEDA-4DB2-BAE9-5FF187463F40.jpg",
+      categoryName: "Pasta",
     },
     {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bd",
+      id: "11",
       image:
-        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-1foodgroups_dairy_detailfeature.jpg?sfvrsn=23510b0_6",
-      categoryName: "Dairy",
+        "https://images.ctfassets.net/3s5io6mnxfqz/5GlOYuzg0nApcehTPlbJMy/140abddf0f3f93fa16568f4d035cd5e6/AdobeStock_175165460.jpeg?fm=jpg&w=900&fl=progressive",
+      categoryName: "Meat",
     },
     {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28be",
+      id: "12",
       image:
-        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-1foodgroups_dairy_detailfeature.jpg?sfvrsn=23510b0_6",
-      categoryName: "Dairy",
+        "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/07/24/fn_bucket-cleaning-supplies_s4x3.jpg.rend.hgtvcom.406.305.suffix/1595872347868.jpeg",
+      categoryName: "Cleaning Supplies",
     },
     {
-      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28bf",
+      id: "13",
       image:
-        "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-1foodgroups_dairy_detailfeature.jpg?sfvrsn=23510b0_6",
-      categoryName: "Dairy",
+        "https://www.newhope.com/sites/newhope360.com/files/natural-personal-care-ew20.jpg",
+      categoryName: "Personal Care",
     },
   ];
 
@@ -88,6 +132,24 @@ const HomeScreen = () => {
     getProducts();
   }, []);
 
+  const filteredProductList = async (categoryName) => {
+    setProducts([]);
+    const q = query(
+      collection(db, "products"),
+      where("stock", ">=", 0),
+      where("publish", "==", true),
+      where("category", "==", categoryName),
+      orderBy("stock", "desc"),
+      orderBy("rating", "desc")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      const docObj = doc.data();
+      docObj.id = doc.id;
+      setProducts((oldArr) => [...oldArr, docObj]);
+    });
+  };
+
   const selectProduct = (item) => {
     navigation.navigate("ProductScreen", { item: item });
   };
@@ -98,12 +160,33 @@ const HomeScreen = () => {
       <View style={tw`bg-black h-1 mt-2`}></View>
       <View style={tw`mt-5 p-3 pl-5`}>
         <Text style={tw`text-black mb-3`}>Categories</Text>
-        <FlatList
-          horizontal
-          data={DATA}
-          renderItem={CategoryComponent}
-          keyExtractor={(item) => item.id}
-        />
+        <ScrollView horizontal={true} style={tw`flex flex-row`}>
+          {DATA.map(({ id, image, categoryName }) => (
+            <TouchableOpacity
+              key={id}
+              onPress={() => {
+                filteredProductList(categoryName);
+              }}
+              style={tw`flex justify-center items-center mr-5`}
+            >
+              <Image
+                style={[
+                  tw`rounded-full mb-2`,
+                  {
+                    width: 55,
+                    height: 55,
+                    resizeMode: "contain",
+                  },
+                ]}
+                resizeMode={"cover"}
+                source={{
+                  uri: image,
+                }}
+              />
+              <Text style={tw`mb-3`}>{categoryName}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
       </View>
       <View style={tw`bg-black h-1 mt-2`}></View>
 
